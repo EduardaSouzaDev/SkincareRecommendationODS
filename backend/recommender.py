60ed225541c0c37065435082417d0642014ab999
Eduarda Souza da Silva. <esds.snf23@uea.edu.br>
2025-09-16 10:37:28 -0400

    Update recommender.py

--- backend/recommender.py
+++ backend/recommender.py
@@ -2,21 +2,26 @@ import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-df = pd.read_csv("backend/data/cosmetic.csv")
+data = pd.read_csv("data/cosmetic.csv")
 
 # Vetorizacao
 vectorizer = CountVectorizer(binary=True, token_pattern=r'[^,]+')
-X = vectorizer.fit_transform(df['ingredients'].fillna(""))
+X = vectorizer.fit_transform(data['ingredients'].fillna(""))
 
 # Similaridade
 similaridade = cosine_similarity(X)
-similaridade = pd.DataFrame(similaridade, index=df['name'], columns=df['name'])
+similaridade = pd.DataFrame(similaridade, index=data['name'], columns=data['name'])
 
 def recommend(nome_produto, top_n=5):
-    if nome_produto not in similaridade.index:
-        return []
 
+    if nome_produto not in similaridade.index:
+        return "Produto não encontrado no dataset."
+
+    # Ordena os produtos pelo grau de similaridade
     similares = similaridade[nome_produto].sort_values(ascending=False)
+
+    # Remove o próprio produto e pega os top-N
     top_produtos = similares.drop(nome_produto).head(top_n)
 
-    return df[df['name'].isin(top_produtos.index)][['name', 'brand', 'ingredients']].to_dict(orient="records")
+    # Retornar informações básicas
+    return data[data['name'].isin(top_produtos.index)][['name', 'brand', 'ingredients']]
