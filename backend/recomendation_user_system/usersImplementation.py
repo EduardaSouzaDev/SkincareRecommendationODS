f7040260f856023af787aabdc3b00799992149d1
RafaelUEA <rdss.snf20@uea.edu.br>
2025-09-27 21:58:08 -0400

    now the usersAvaliation csv is filled correctly randomly

--- backend/recomendation_user_system/usersImplementation.py
+++ backend/recomendation_user_system/usersImplementation.py
@@ -2,11 +2,14 @@ import pandas as pd
 import random as rd
 import time
 import numpy as np
+import sys
 
+
+sys.stdout.reconfigure(encoding="utf-8")
 rd.seed(time.time_ns())
 
 df_users = pd.read_csv("backend/data/usersAvaliation.csv")
-df_ingredients = pd.read_csv("backend/data/cosmetic.csv")
+df_ingredients = pd.read_csv("backend/data/cosmetic.csv", encoding="utf-8")
 
 MAX = 6
 MIN = 0
@@ -17,17 +20,28 @@ ID = df_ingredients["id"]
 def generate_new_ratings():
     rating = np.random.randint(low=MIN, high=MAX, size=len(df_users))
     df_users["rate"] = rating
+    #df_users.to_csv("usersAvaliation.csv", index=False)
+
+def generate_cosmetics():
+    id = np.random.randint(low=COS_MIN, high=COS_MAX, size=(len(df_users)))
+    return id
+
+def fill_csv():
+    generate_new_ratings()
+    cosmetic = []
+    cosmetics_id = np.random.randint(low=COS_MIN, high=COS_MAX, size=(len(df_users)))
+    for id in cosmetics_id:
+        cosmetic.append(df_ingredients["name"][id])
+    df_users["cosmetic"] = cosmetic
+    print(df_users)
     df_users.to_csv("backend/data/usersAvaliation.csv", index=False)
 
 
-def generate_cosmetics():
-    
-    return id
-
 
 #generate_new_ratings()
-
-print(df_users)
+fill_csv()
+#print(chr(8482))
+#print(df_users)
 
 #print(rand_number)
 
